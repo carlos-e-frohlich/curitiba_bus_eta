d6af74186a66916509fdc5f9754332012dae6174
Carlos Eduardo Fröhlich <carlos.e.frohlich@gmail.com>
2024-10-15 19:46:11 -0300

    Deploy collection of bus locations
    The scripts for fetching live bus locations from the API and inserting them into
    a BigQuery table have been successfully deployed to Google Cloud Platform.

---
+++ cloud/ingest_locations/main.py
@@ -0,0 +1,16 @@
+import pandas_gbq
+from fetch import fetch_set_of_locations
+
+
+def append_locations(message=None, context=None):
+    set_of_locations = fetch_set_of_locations(route_number='216')
+
+    pandas_gbq.to_gbq(
+        set_of_locations,
+        'locations.locations',
+        project_id='curitiba-bus-eta',
+        if_exists='append'
+    )
+
+if __name__ == '__main__':
+    append_locations()
