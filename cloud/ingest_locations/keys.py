b4a0c1452f2a9b4022c7e21c4f6a406d760c18fe
Carlos Eduardo Fröhlich <carlos.e.frohlich@gmail.com>
2024-10-15 19:14:56 -0300

    Point to Google Cloud secret

--- cloud/ingest_locations/keys.py
+++ cloud/ingest_locations/keys.py
@@ -6,5 +6,5 @@ client = secretmanager.SecretManagerServiceClient()
 
 # Get the API key for bus data.
 
-response_api_key_bus_data = client.get_secret(request={"name": "projects/755625127439/secrets/api_keys"})
+response_api_key_bus_data = client.get_secret(request={"name": "projects/441247924338/secrets/api_keys"})
 api_key_bus_data = response_api_key_bus_data.labels['api_curitiba_156']
